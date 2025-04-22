892bbca578ee6b42a77a46ee60c2c15739b293af
Kleyton Luiz <kleytonl.vieira@gmail.com>
2025-04-22 13:15:45 -0300

    Menus e ArrayList

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,26 +1,61 @@
 package app;
 
 import java.util.Scanner;
+import java.util.List;
+import java.util.ArrayList;
 import model.*;
 
 public class Main {
     public static void main(String[] args) {
 
+        int opt;
+
         Scanner sc = new Scanner(System.in);
+        List<Afazeres> listas = new ArrayList<>();
 
-        String nome, descricao;
+        System.out.println("----- Lista de Afazeres! -----");
 
-        System.out.println("Lista de Afazeres!");
+        do {
+            System.out.println("\n------- Menu principal -------");
+            System.out.println("------------------------------");
+            System.out.println("--- 1 - Criar nova lista -----");
+            System.out.println("----- 2 - Deletar lista ------");
+            System.out.println("---- 3 - Mostrar listas ------");
+            System.out.println("---------- 0 - Sair ----------");
+            System.out.println("------------------------------");
+            System.out.print("Escolha uma alternativa: ");
+            opt = sc.nextInt();
 
-        System.out.println("Escolha o nome da sua lista: ");
-        nome = sc.nextLine();
+            switch(opt) {
+                case 1:
+                    System.out.println("Defina seu objetivo: ");
+                    sc.nextLine();
+                    String objetivo = sc.nextLine();
 
-        System.out.println("Escreva a descrição da lista");
-        descricao = sc.nextLine();
+                    System.out.println("Defina a meta: ");
+                    String meta = sc.nextLine();
 
-        Afazeres lista1 = new Afazeres(nome, descricao);
+                    listas.add(new Afazeres(objetivo, meta));
 
-        System.out.println(lista1);
+                    System.out.println("Afazer adicionado a lista...");
+                    break;
+                case 2:
+                    System.out.println("Escolha a lista que deseja deletar: ");
+                    int i = sc.nextInt();
+                    listas.remove((i-1));
 
+                    System.out.println("Afazer removido da lista...");
+                    break;
+                case 3:
+                    for(Afazeres l : listas) {
+                        System.out.println(l);
+                    }
+                    break;
+                case 0:
+                    System.out.println("Terminando programa...");
+                    break;
+            }
+
+        } while (opt != 0);
     }
 }
